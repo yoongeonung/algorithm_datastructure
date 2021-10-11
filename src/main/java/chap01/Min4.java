4e49996678a00871e7bc00c3edd87e65fe578622
yoongeonung <yoongeonung@gmail.com>
2021-10-11 17:38:44 +0900

    feat : Chap1 - Q3

---
+++ src/main/java/chap01/Min4.java
@@ -0,0 +1,28 @@
+package chap01;
+
+public class Min4 {
+    static int min4(int a, int b, int c, int d) {
+        int min = a;
+        if (b < min) {
+            min = b;
+        }
+        if (c < min) {
+            min = c;
+        }
+        if (d < min) {
+            min = d;
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+        System.out.println("네 정수의 최솟값을 구합니다.");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr[j] = (int)(Math.random() * 9) + 1;
+            }
+            System.out.printf("min(%d, %d, %d, %d) = %d \n ", arr[0], arr[1], arr[2], arr[3], min4(arr[0], arr[1], arr[2],arr[3]));
+        }
+    }
+}
