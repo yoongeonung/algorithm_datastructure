31b0476ec592e92c1d03ab1e70bbaa0234286086
yoongeonung <yoongeonung@gmail.com>
2021-10-20 19:00:19 +0900

    feat : Chap1 - Q10

--- src/main/java/chap01/practice/Q8.java
+++ src/main/java/chap01/practice/Q8.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Q8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("1부터 n까지의 합을 구합니다.");
         System.out.print("n의 값 : ");
         int n = scanner.nextInt();
         // Gauss
