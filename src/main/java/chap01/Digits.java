7ad3f8af2be33c24081d478260630df0b79565c4
yoongeonung <yoongeonung@gmail.com>
2021-11-16 16:56:35 +0900

    style : Adjust De Morgan's law

--- src/main/java/chap01/Digits.java
+++ src/main/java/chap01/Digits.java
@@ -14,6 +14,15 @@ public class Digits {
         do {
             System.out.print("입력 : ");
             num = Integer.parseInt(br.readLine());
-        } while (num < 10 || num > 99);
+        } while (!(num >= 10 && num <= 99));
+
+        // num < 10 || num > 99 드모르간 법칙 적용 전
+        // !(num >= 10 && num <= 99) 드모르간 법칙 적용
+        /*
+         * 드모르간의 법칙
+         * 각 조건을 부정하고, 논리합(||)을 논리곱(&&)으로
+         * 또는 논리곱을, 논리합으로 바꾸고 다시 전체를 부정하면
+         * 원래의 조건과 같다는 법칙
+         */
     }
 }
