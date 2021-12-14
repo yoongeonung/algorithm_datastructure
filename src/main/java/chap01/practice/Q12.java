2eb8da47bc8869424182e60589d88fbc6ec48261
yoongeonung <yoongeonung@gmail.com>
2021-12-14 23:44:54 +0900

    feat : Chap1 - Q12

--- src/main/java/chap01/practice/Q12.java
+++ src/main/java/chap01/practice/Q12.java
@@ -2,13 +2,35 @@ package chap01.practice;
 
 public class Q12 {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
-                stringBuilder.append(String.format("%3d", i * j));
-            }
-            stringBuilder.append("\n");
-        }
+        /*
+         * 연습문제Q12
+         * 위쪽과 왼쪽에 곱하는 수가 있는 곱셈표를 출력하시오.
+         * 구분선은 수직선 기호(|), 마이너스 기호(-), 플러스 기호(+)를 사용하시오.
+         */
 
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < 12; i++) {
+            if (i == 1) {
+                sb.append(String.format("%3s", ""));
+                sb.append(String.format("%3s", "|"));
+            } else if (i == 2) {
+                sb.append(String.format("%3s", "-"));
+                sb.append(String.format("%3s", "+"));
+            } else {
+                sb.append(String.format("%3d", i - 2));
+                sb.append(String.format("%3s", "|"));
+            }
+            for (int j = 1; j < 10; j++) {
+                if (i == 2) {
+                    sb.append(String.format("%3s", "-"));
+                } else if (i > 2) {
+                    sb.append(String.format("%3d", (i - 2) * j));
+                } else {
+                    sb.append(String.format("%3d", i * j));
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
     }
 }
