27579a083e0a2c723fabebb8a800a13f4129a655
yoongeonung <yoongeonung@gmail.com>
2022-01-15 21:41:49 +0900

    feat : Chap1 - Q14

--- src/main/java/chap01/practice/Q14.java
+++ src/main/java/chap01/practice/Q14.java
@@ -1,7 +1,20 @@
 package chap01.practice;
 
-public class Q14 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Q14 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder strb = new StringBuilder();
+        int number = Integer.parseInt(br.readLine());
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < number; j++) {
+                strb.append("*");
+            }
+            strb.append("\n");
+        }
+        System.out.println(strb);
     }
 }
