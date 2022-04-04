445d6a105bf05a67ed0990a06f83acc005bab955
yoongeonung <yoongeonung@gmail.com>
2022-04-04 14:36:54 +0900

    feat : No side effects

---
+++ everyone_datastructure_algorithm/src/main/java/chap3/SideEffects.java
@@ -0,0 +1,19 @@
+package chap3;
+
+public class SideEffects {
+
+    private Cryprographer cryprographer;
+    // 부수 효과를 일으키는 안좋은 함수
+    public boolean checkPassword(String userName, String password) {
+        User user = UserGateway.findByName(userName);
+        if (user != User.NULL){
+            String codedPhrase = user.getPhraseEncodedByPassword();
+            String phrase = cryptographer.decrypt(codedPhrase, password);
+            if ("Valid Password".equals(phrase)) {
+                Session.initialize();
+                return true;
+            }
+        }
+        return false;
+    }
+}
